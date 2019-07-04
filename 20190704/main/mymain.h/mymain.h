1e7d36216e82adbf2a1bdd6b7ae29adf117c5ce7
StellarChen <StellarChen@users.noreply.github.com>
2019-07-04 21:08:31 +0800

    1

---
+++ 20190704/main/mymain.h/mymain.h
@@ -0,0 +1,8 @@
+#ifndef __MYMAIN_H
+#define __MYMAIN_H
+
+#include "stm32h7xx_hal.h"
+
+void Mymain(void);
+
+#endif 
---
+++ 20190704/main/mymain_c/mymain.c
@@ -0,0 +1,14 @@
+#include "mymain.h" 
+
+
+
+void Mymain()
+{
+	while(1)
+	{
+	  HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_7);
+		HAL_Delay (500);
+	
+	
+	}
+}
